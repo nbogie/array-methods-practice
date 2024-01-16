676a0a943ea171cb08af81f1fb578426becb788c
nbogie <neillbogie@googlemail.com>
2024-01-16 14:52:31 +0000

    finish soln

--- src/conversion/myFunctions.js
+++ src/conversion/myFunctions.js
@@ -1,63 +1,94 @@
 //In all of these examples, a "long" string should be taken to mean
 // any string which has a length of 7 or more.
 
+function isLongString(s) {
+    return s.length > 6;
+}
+
+function isOdd(n) {
+    return n % 2 !== 0;
+}
+
 /** return the first "long" string in the given array, or undefined if none is found.
  * @param {string[]} inputStrings
  * @return {string|undefined}
  */
-function findFirstLongString(inputStrings) {}
+function findFirstLongString(inputStrings) {
+    return inputStrings.find(isLongString);
+}
 
 /** return the first odd number in the given array of numbers, or undefined if none is found.
  * @param {number[]} inputNumbers
  * @return {number|undefined}
  */
-function findFirstOddNumber(inputNumbers) {}
+function findFirstOddNumber(inputNumbers) {
+    return inputNumbers.find((n) => n % 2 !== 0);
+}
 
 /** return the LAST "long" string in the given array of strings, or undefined if none is found.
  * @param {string[]} inputStrings
  * @return {string|undefined}
  */
-function findLastLongString(inputStrings) {}
+function findLastLongString(inputStrings) {
+    return inputStrings.findLast(isLongString);
+}
 
 /** Given an array of strings of various lengths,
  * return a new array containing only the "long" strings from the input array
  * @param {string[]} inputStrings
  * @returns {string[]}
  * */
-function findAllLongStrings(inputStrings) {}
+function findAllLongStrings(inputStrings) {
+    return inputStrings.filter(isLongString);
+}
 
 /** given an array of numbers, return a new array containing all the odd numbers in the input array.  Returns an empty array if none are found.
  * @param {number[]} inputNumbers
  * @return {number[]}
  */
-function findAllOddNumbers(inputNumbers) {}
+function findAllOddNumbers(inputNumbers) {
+    return inputNumbers.filter(isOdd);
+}
 
+function isString(item) {
+    return typeof item === "string";
+}
 /** Given an array of mixed types of value
  * return a new array containing only those elements of type string.
  * @param {any[]} inputArray
  * @returns {string[]}
  * */
-function findAllTheStrings(inputStrings) {}
+function findAllTheStrings(inputArray) {
+    return inputArray.filter(isString);
+}
 
 /** Given an array of people objects,
  * return a new array containing only those people objects whose firstName OR secondName (or both) is long.
- * @param {{firstName: string, secondName:string}[]} inputArray
+ * @param {{firstName: string, secondName:string}[]} inputObjects
  * @returns {{firstName: string, secondName:string}[]}
  * */
-function findAllPeopleWithALongName(inputObjects) {}
+function findAllPeopleWithALongName(inputObjects) {
+    return inputObjects.filter(
+        (o) => isLongString(o.firstName) || isLongString(o.secondName)
+    );
+}
 
 /** given an array of numbers return true if all of them are odd, else false.
  * @param {number[]} inputNumbers
  * @return {boolean}
  */
-function areAllNumbersOdd(inputNumbers) {}
+function areAllNumbersOdd(inputNumbers) {
+    return inputNumbers.every(isOdd);
+}
 
 /** Given an array of strings of various lengths,
  * return true if all of the strings are "long", else false
  * @param {string[]} inputStrings
  * @returns {boolean}
  * */
-function areAllStringsLong(inputStrings) {}
+function areAllStringsLong(inputStrings) {
+    return inputStrings.every(isLongString);
+}
 
 export {
     findFirstLongString,
